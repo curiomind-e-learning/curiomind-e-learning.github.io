83e0b923347a696f12955f2ab32d92877e712e84
avinashkranjan <ranjan.avinash@hotmail.com>
2021-12-08 02:08:57 +0530

    react tailwind setup

---
+++ curiomind-e-learning.github.io/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <section className="App h-screen w-full flex justify-center items-center bg-green-500">
+        <p className="text-7xl">Curiomind</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
