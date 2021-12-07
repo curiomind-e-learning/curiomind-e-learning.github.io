2b8d784679a14f3fe95bc85c978e372d6eedeec7
Avinash Ranjan <ranjan.avinash@hotmail.com>
2021-12-08 02:20:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <section className="App h-screen w-full flex justify-center items-center bg-green-500">
-        <p className="text-7xl">Curiomind</p>
+        <p className="text-7xl">🍁 Curiomind</p>
       </section>
     </div>
   );
