c9645d4efc0a277c05bceef3c07a9e5b179dbfb2
Avinash Ranjan <ranjan.avinash@hotmail.com>
2022-10-01 00:31:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <section className="App h-screen w-full flex justify-center items-center bg-green-500">
         <p className="text-7xl">🍁 Curiomind</p>
+     <p className="text-3xl">An excellece center for education</p>
       </section>
     </div>
   );
